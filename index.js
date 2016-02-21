a3790407743925a65c1422b0a5f71f043ac2a04f
Abin Pal <abinpal1989@gmail.com>
2016-02-21 16:59:30 +0530

    Basic Code

---
+++ index.js
@@ -0,0 +1,51 @@
+
+var lineReader = require('line-reader');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var Promise = require('bluebird');
+
+var moduleArr = []; 
+var fileName = process.argv[2];
+
+function storingModuleNames(){
+
+var eachLine = Promise.promisify(lineReader.eachLine);
+
+    eachLine(fileName, function(line) {
+      if (line.indexOf('require') != -1){
+      var regex = /require\(([^)]+)\)/g;
+      var mat = regex.exec(line);
+
+      mat[1] = mat[1].replace(/'/g, '');
+      moduleArr.push(mat[1]);
+    }
+    }).then(function() {
+      runningCommand(moduleArr);
+    }).catch(function(err) {
+      console.error(err);
+    });
+};
+
+function runningCommand(modules){
+    for (var module in modules){
+
+      var globalCommand = 'npm install -g '+modules[module];
+      var localCommand = 'npm install '+modules[module]+' --save';
+      
+      function puts(error, stdout, stderr) { sys.puts(stdout) }
+      exec(globalCommand, puts);
+      exec(localCommand, puts);
+    }
+  };
+
+var compute = function() {
+
+  return new Promise(function(resolve) {
+            storingModuleNames();
+        })
+        .catch(function (e) {
+            throw e;
+        });
+};
+
+compute();
